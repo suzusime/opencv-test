2422c465457d9fd79f9fd96e80226b148317622b
suzusime <tamagawajousuiro+git@gmail.com>
2018-09-16 15:42:11 +0900

    顔認識できるようになった

--- main.cpp
+++ main.cpp
@@ -1,32 +1,38 @@
 #include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 
-int showimg(int argc, const char* argv[])
+using namespace std;
+using namespace cv;
+
+void detectAndDisplay( Mat frame );
+
+String face_cascade_name, eyes_cascade_name;
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+String window_name = "Capture - Face detection";
+
+int main(int argc, char* argv[])
 {
-  // 画像データをファイルから読み込む
-  cv::Mat src = cv::imread("chino.png", cv::IMREAD_COLOR);
+    CommandLineParser parser(argc, argv,
+        "{help h||}"
+        "{face_cascade|/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml|}"
+        "{eyes_cascade|/usr/share/opencv/haarcascades/haarcascade_eye_tree_eyeglasses.xml|}");
+    parser.about( "\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
+                  "You can use Haar or LBP features.\n\n" );
+    parser.printMessage();
 
-  // 画像の読み込みに失敗したらエラー終了する
-  if(src.empty())
-  {
-    std::cerr << "Failed to open image file." << std::endl;
-    return -1; 
-  }
+    face_cascade_name = parser.get<String>("face_cascade");
+    eyes_cascade_name = parser.get<String>("eyes_cascade");
 
-  cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
-  cv::imshow("image", src);
-  cv::waitKey(0);
-  cv::destroyAllWindows();
+    //-- 1. Load the cascades
+    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
+    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading eyes cascade\n"); return -1; };
 
-  return 0;
-}
-
-#include "opencv2/opencv.hpp"
-
-int main(int argh, char* argv[])
-{
     cv::VideoCapture cap(0);//デバイスのオープン
     //cap.open(0);//こっちでも良い．
 
@@ -44,8 +50,10 @@ int main(int argh, char* argv[])
         //
         //取得したフレーム画像に対して，クレースケール変換や2値化などの処理を書き込む．
         //
+        detectAndDisplay( frame );
 
-        cv::imshow("window", frame);//画像を表示．
+
+        //cv::imshow("window", frame);//画像を表示．
 
         int key = cv::waitKey(1);
         if(key == 113)//qボタンが押されたとき
@@ -60,4 +68,31 @@ int main(int argh, char* argv[])
     }
     cv::destroyAllWindows();
     return 0;
+}
+
+void detectAndDisplay( Mat frame )
+{
+    std::vector<Rect> faces;
+    Mat frame_gray;
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+    //-- Detect faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(60, 60) );
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        Mat faceROI = frame_gray( faces[i] );
+        std::vector<Rect> eyes;
+        //-- In each face, detect eyes
+        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+        for ( size_t j = 0; j < eyes.size(); j++ )
+        {
+            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        }
+    }
+    //-- Show what you got
+    imshow( window_name, frame );
 }
